28b8ed738dae4e055d82d70296f061b1f9b98395
Eslam Elsayed <128435971+eeslam99@users.noreply.github.com>
2025-04-22 14:57:57 +0200

    Update Station.cs

--- Domain/Entities/Station.cs
+++ Domain/Entities/Station.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public partial class Station
+    public class Station
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
