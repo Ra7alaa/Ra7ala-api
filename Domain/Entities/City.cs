9e6f807ced7be246b91454ae0a13da7f83918a73
Eslam Elsayed <128435971+eeslam99@users.noreply.github.com>
2025-04-22 14:55:16 +0200

    Update City.cs

--- Domain/Entities/City.cs
+++ Domain/Entities/City.cs
@@ -7,21 +7,14 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public partial class City
+    public class City
     {
         public int Id { get; set; }
-
-        [StringLength(20)]
-        [Required]
         public string Name { get; set; } = string.Empty;
-
-        [StringLength(20)]
-        [Required]
         public string Governorate { get; set; } = string.Empty;
         public bool IsDeleted { get; set; } = false;
 
         // Navigation properties
-        
         public ICollection<Station> Stations { get; set; } = new List<Station>();
     }
 }
